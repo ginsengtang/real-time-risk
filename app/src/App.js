c141e8c82fb64dfa0d5ca0998b1f7b84c8278c3a
George Tang <gtang.gt@gmail.com>
2021-06-27 22:15:33 -0500

    app

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Field } from "./components/field/field.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Field></Field>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/field/field.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class Field extends React.PureComponent {
+  render() {
+    return (
+      <div style={{ width: "50px", height: "50px", background: "red" }}></div>
+    );
+  }
+}
